b04e599590587971e454369557d3e8616ca06b08
Radu Surducan <rsurducan@gmail.com>
2015-09-27 20:11:00 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Code goes here
+
+angular.module('myApp', [])
+.controller('myCtrl', function($scope) {
+  $scope.FemaleNamePH='Female name';
+  $scope.JobTitlePH='Job title';
+  $scope.TediousTaskPH='Tedious task';
+  $scope.DirtyTaskPH='Dirty task';
+  $scope.CelebrityPH='Celebrity';
+  $scope.UselessSkillPH='Useless skill';
+  $scope.ObnoxiousCelebrityPH='Obnoxious celebrity';
+  $scope.HugeNumberPH='Huge number';
+  $scope.AdjectivePH='Adjective';
+});
