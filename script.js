6dc21b54acd4a9d74bb4b11744f9230ddb3791d8
Radu Surducan <rsurducan@gmail.com>
2015-10-06 07:39:13 -0400

    I implemented hide / show. I added some basic validation.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 // Code goes here
 angular.module('myApp', [])
 .controller('myCtrl', function($scope) {
-  $scope.FemaleNamePH='Female name';
-  $scope.JobTitlePH='Job title';
-  $scope.TediousTaskPH='Tedious task';
-  $scope.DirtyTaskPH='Dirty task';
-  $scope.CelebrityPH='Celebrity';
-  $scope.UselessSkillPH='Useless skill';
-  $scope.ObnoxiousCelebrityPH='Obnoxious celebrity';
-  $scope.HugeNumberPH='Huge number';
-  $scope.AdjectivePH='Adjective';
+
+	$scope.FemaleNamePH='Female name';
+	$scope.JobTitlePH='Job title';
+	$scope.TediousTaskPH='Tedious task';
+	$scope.DirtyTaskPH='Dirty task';
+	$scope.CelebrityPH='Celebrity';
+	$scope.UselessSkillPH='Useless skill';
+	$scope.ObnoxiousCelebrityPH='Obnoxious celebrity';
+	$scope.HugeNumberPH='Huge number';
+	$scope.AdjectivePH='Adjective';
+
+	$scope.generator = true;
+
+	$scope.data = {}
+
+	$scope.generateMadLib = function() {
+		$scope.generator = false;  
+	};
+
+	$scope.startOver = function() {
+		$scope.generator = true;
+		$scope.data = {}
+	}
+
 });
